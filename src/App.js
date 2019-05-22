fc806c043b91781aa9ae612c32c151cc8f08a220
Test test <test@test.com>
2019-05-22 16:47:13 +0200

    SetInterval counter and MongoDB added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
+import mongodb from './mongodb.svg';
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 //Importamos nuestro componente
 
-import Hola from './Hola';
 import Contador from './Contador';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <div className="App-intro">
-        {/*Incluimos nuestro componente*/}
-        <Hola nombre="Prueba"/>
+        <div class="row">
+          <div class="column">
+            <img src={logo} className="App-logo" alt="logo" />
+          </div>
+          <div class="column">
+            <img src={mongodb} className="Mongodb-logo" alt="logo" />
+          </div>
+        </div>
         <Contador/>
-      </div>
+      </header>
     </div>
   );
 }
--- src/Contador.js
+++ src/Contador.js
@@ -5,25 +5,63 @@ class Contador extends Component {
         super();
 
         this.state = {
+            data: [],
             contador: 0
         };
     }
 
-    aumentarContador = () => {
-        // Importante no modificar directamente el estado, si no usar setState
-        // y pasarle la clave del objeto a modificar y su nuevo valor
-        this.setState({contador: this.state.contador + 1});
+    // aumentarContador = () => {
+    //     // Importante no modificar directamente el estado, si no usar setState
+    //     // y pasarle la clave del objeto a modificar y su nuevo valor
+    //     this.setState({contador: this.state.contador + 1});
+    // }
+
+    componentDidMount() {
+        let interval = setInterval(() => {
+            
+            // let date = new Date().getDate();
+            // let month = new Date().getMonth();
+            // let year = new Date().getFullYear();
+            // let hours = new Date().getHours();
+            // let min = new Date().getMinutes();
+            // let sec = new Date().getSeconds();
+            // this.setState ({date: date + "/" + month + "/" + year + " " + hours + ":" + min + ":" + sec});
+
+            (this.state.contador === 100 ? (clearInterval(interval)) : this.setState({ contador: this.state.contador + 1 }))
+        }, 1000);
+        this.getDataFromDb();
     }
+
+    getDataFromDb = () => {
+        fetch('http://localhost:3001/api/getData')
+          .then((data) => data.json())
+          .then((res) => this.setState({ data: res.data }));
+      };
 
     render() {
         return (
+            (this.state.contador === 100 ? <h1>Cuenta terminada!</h1> : this.renderContent())
+        )
+    }
+
+    renderContent() {
+        const { data } = this.state;
+        return (
+            
             /* Como veiamos antes es necesario devolver un unico elemento padre
             por eso usamos un div para agrupar los demas elementos */
             <div>
+                <ul>
+                {data.length <= 0
+                    ? 'NO DB ENTRIES YET'
+                    : data.map((dat) => (
+                        <div>
+                        <h1>Hola {dat.name}</h1>
+                        </div>
+                    ))}
+                </ul>
+                <h3>{this.state.date}</h3>
                 <span>{this.state.contador}</span>
-                {/* Al metodo onClick le asignamos un metodo.
-                Importante poner la C de onClick en mayusculas */}
-                <button onClick={this.aumentarContador}>+</button>
             </div>
         );
     }
